ae57f86632b3381b8ba4170798919043aba5e890
Michael Montaña <michaelmontana@me.com>
2024-10-18 15:39:15 +0800

    fix: model and serializers

--- weekly_sched/models.py
+++ weekly_sched/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
-
 class TimeSlot(models.Model):
     day = models.CharField(max_length=9)
     start = models.TimeField()
     stop = models.TimeField()
-    ids = models.CharField(max_length=50)
-
+    ids = models.JSONField(default=dict)
 
     def __str__(self):
         return f'{self.day} {self.start}-{self.stop} ({self.ids})'
--- weekly_sched/serializers.py
+++ weekly_sched/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import TimeSlot
 
-
 class TimeSlotSerializer(serializers.ModelSerializer):
     class Meta:
         model = TimeSlot
         fields = '__all__'
+
+    def validate_ids(self, value):
+        if not isinstance(value, dict):
+            raise serializers.ValidationError("The 'ids' field must be a dictionary.")
+        return value
